d91331bf7e0eb325ce7546610f8ecbd917822936
Benjamin Kinga <somethingscripted@gmail.com>
2021-11-30 23:10:42 -0600

    python day1

---
+++ day01.py
@@ -0,0 +1,18 @@
+# open the 01_input.txt file and read the content
+with open('01_input.txt') as f:
+    content = [int(i.rstrip()) for i in f.readlines()]
+
+
+# Part 1
+count = 0
+for i in range(1, len(content)):
+    if content[i] > content[i-1]:
+        count += 1
+print(count)
+
+# Part 2
+count = 0
+for i in range(1, len(content) - 2):
+    if sum(content[i:i+3]) > sum(content[i-1:i+2]):
+        count += 1
+print(count)
